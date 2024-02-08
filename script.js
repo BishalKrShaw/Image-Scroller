28b86f04c81e5424fb74052d28d86c317c0c6260
BishalKrShaw <bishalshaw.mfis@gmail.com>
2024-02-08 15:30:55 +0530

    Feature in process

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 const images = document.querySelectorAll(".image-container img");
-let imgArr = Array.from(images);
-console.log(imgArr);
+const slider = document.querySelector(".slider");
+const leftBtn = document.querySelector("#leftBtn");
+const rightBtn = document.querySelector("#rightBtn");
+
+let imageNumber = 1;
+let imgLength = images.length;
+
+// Right Arrow Function
+function rightSlider() {
+    if(imageNumber < imgLength){
+        slider.style.transform = `translateX(-${imageNumber * 350}px)`;
+        imageNumber++;
+    }
+    if(imageNumber == imgLength) {
+        imageNumber = 0;
+    }
+}
+
+rightBtn.addEventListener("click", rightSlider);
+
+// Left Arrow Function
+function leftSlider() {
+    console.log(imgLength);
+    if(imgLength) {
+        slider.style.transform = `translateX(-${(imgLength - 1) * 350}px)`;
+        imgLength--;
+    }
+    if(imgLength < 1){
+        imgLength = images.length;
+    }
+}
+
+leftBtn.addEventListener("click", leftSlider);
